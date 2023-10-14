3bd5771a662fcf020d406b89c8f691ed7680c4df
Ayoub-Haj-Husein <ayoubabdulrahmanhusein@gmail.com>
2023-10-14 09:07:12 +0300

    add login and register pages

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:3000/comments")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.log("there is an error with git"));
-});
 
-let arr = [1, 2, 5, 8];
-
-console.log(
-  arr.filter((el) => {
-    return el <= 2;
-  })
-);
